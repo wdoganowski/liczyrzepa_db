f794cde8f6b67e939a3ca34dd8f551ccb112e74d
Wojtek Doganowski <wojciech@doganowscy.com>
2023-01-05 12:33:14 +0100

    added ec2 stripts

---
+++ ec2/ec2-user-data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+sudo apt-get update
+
+apt install unzip
+
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+apt-get install -y nodejs
+
+apt-get update
+apt-get upgrade
+
+wget https://aws-tc-largeobjects.s3-us-west-2.amazonaws.com/ILT-TF-100-TECESS-5/app/app.zip
+mkdir -p /var/app
+unzip app.zip -d /var/app
+
+cd /var/app
+npm install
+
+cd
+wget https://github.com/node-app
+cp node-app /etc/init.d
+/sbin/update-rc.d node-app defaults
+
+cd /var/app
+npm start
